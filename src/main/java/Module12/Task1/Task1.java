d60f8235134748b9295d83190b275a5323136b93
Vlad2344fverkvw3 <vladreznik954@gmail.com>
2023-03-03 23:37:36 +0200

    Task1 and Task2

---
+++ src/main/java/Module12/Task1/Task1.java
@@ -0,0 +1,25 @@
+package Module12.Task1;
+
+public class Task1 {
+
+        private  int second = 0;
+        public void performed() {
+            while (second < 20) {
+                for (int i = 0; i < 5; i++) {
+                    second++;
+                    System.out.printf("Time : %d seconds\n", second);
+                }
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    System.out.println("Error!");
+                }
+                theAmountOfTimeThatHasPassed();
+            }
+        }
+
+        public void theAmountOfTimeThatHasPassed() {
+            if (second != 1)
+                System.out.printf("%d seconds have passed!\n", second);
+        }
+}
---
+++ src/main/java/Module12/Task2/Task2.java
@@ -0,0 +1,92 @@
+package Module12.Task2;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+
+
+public class Task2 {
+    private volatile int number = 1;
+
+    public final Queue<String> queue = new LinkedList<>();
+
+
+    public synchronized void number() {
+
+        while (number <= 15) {
+            if (number % 3 != 0 && number % 5 != 0) {
+                queue.add(String.valueOf(number));
+                number++;
+                notifyAll();
+            } else {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Error number method");
+                }
+            }
+        }
+    }
+
+    public synchronized void fizz() {
+        while (number <= 15) {
+            if (number % 3 == 0 && number%5 !=0) {
+                queue.add("fizz");
+                number++;
+                notifyAll();
+            } else {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Error fizz method");
+                }
+            }
+
+        }
+
+    }
+
+    public synchronized void buzz() {
+
+        while (number < 15) {
+            if (number % 5 == 0 ) {
+                queue.add("buzz");
+                number++;
+                notifyAll();
+            } else {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Error buzz method");
+                }
+            }
+        }
+
+    }
+
+    public synchronized void fizzbuzz() {
+
+        while (number <= 15) {
+            if (number % 5 == 0) {
+                queue.add("fizzbuzz");
+                number++;
+                notifyAll();
+            } else {
+                try {
+                    wait();
+                } catch (InterruptedException e) {
+                    System.out.println("Error fizzbuss method");
+                }
+            }
+        }
+
+    }
+
+
+
+    }
+
+
+
